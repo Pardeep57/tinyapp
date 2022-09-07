9b8abc578b34ff7702b8b27662c13ff43ffae1e5
Pardeep57 <pardeepofficial57@gmail.com>
2022-09-07 00:44:16 +0000

    /set /fetch tested

--- express_server.js
+++ express_server.js
@@ -22,4 +22,11 @@ app.get("/urls.json", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
 
-  
+  app.get("/set", (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+   });
+   
+   app.get("/fetch", (req, res) => {
+    res.send(`a = ${a}`);
+   });
