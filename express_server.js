25a36828c4ae027e524b666db628d877112f750f
Pardeep57 <pardeepofficial57@gmail.com>
2022-09-10 06:24:31 +0000

    morgan installed

--- express_server.js
+++ express_server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const morgan = require('morgan');
+
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = process.env.PORT || 8080; // default port 8080
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 
@@ -22,54 +27,61 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-/*
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-*/
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-app.get("/hello", (req, res) => {
-    const templateVars = { greeting: "Hello World!" };
-    res.render("hello_world", templateVars);
-  });
 
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-  app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
-    res.render("urls_show", templateVars);
-  });
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: "http://www.lighthouselabs.ca",
+  };
+  res.render("urls_show", templateVars);
+});
 
-  app.get("/u/:id", (req, res) => {
-   // const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
-   // res.render("urls_show", templateVars);
-   const longURL = urlDatabase[req.params.id];
-   res.redirect(longURL);
+app.get("/u/:id", (req, res) => {
+  // const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+  // res.render("urls_show", templateVars);
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
 
-  });
-  app.post("/urls/:id/delete", (req, res) => {
-    const urlToDelete = req.params.id;
-    delete urlDatabase[urlToDelete];
-    res.redirect('/urls');
-  });
+app.post("/urls/:id/delete", (req, res) => {
+  const urlToDelete = req.params.id;
+  delete urlDatabase[urlToDelete];
+  res.redirect("/urls");
+});
 
-  app.post("/urls", (req, res) => {
-    console.log(req.body); // Log the POST request body to the console
-    res.send("Ok"); // Respond with 'Ok' (we will replace this)
-  });
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
-  function generateRandomString() {}
+const generateRandomString = () => {
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const upperCase = lowerCase.toUpperCase();
+  const numeric = "1234567890";
+  const alphaNumeric = lowerCase + upperCase + numeric;
+  //alphaNumeric is 62
+  let index = Math.round(Math.random() * 100);
+  if (index > 61) {
+    while (index > 61) {
+      index = Math.round(Math.random() * 100);
+    }
+  }
+  return alphaNumeric[index];
+};
+
+const randomString = () => {
+  let randomString = "";
+  while (randomString.length < 6) {
+    randomString += generateRandomString();
+  }
+  return randomString;
+};
