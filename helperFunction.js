068c11f31439826b2c1db5ef8357d50c59364f95
Pardeep57 <pardeepofficial57@gmail.com>
2022-09-13 03:58:33 +0000

    some more changes

---
+++ helperFunction.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+
+const getUserByEmail = (newVal, database) => {
+  for (let user in database) {
+    if (database[user].email === newVal) {
+      return false;
+    }
+  }
+  return true;
+  };
+  module.exports = { getUserByEmail };
